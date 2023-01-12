e7bc2cabf0d2cb94e605bf6bb402d8d9868bb470
marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2023-01-12 19:17:51 +0000

    Add nigth_mode

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const body = document.getElementsByTagName("body")[0];
 
 const changeColor = ()=>{
     body.classList.toggle("change-body");
+    nigthMode.classList.toggle("fa-moon");
 }
 
 nigthMode.addEventListener('click',changeColor);
